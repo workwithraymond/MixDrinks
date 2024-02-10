ff7f0464d58857dff34aebe41d8bed4817c403b7
workwithraymond <workwithraymond01@gmail.com>
2024-02-10 09:33:54 -0500

    new commit

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, Link} from 'react-router-dom';
+import { useLoaderData, Link, Navigate} from 'react-router-dom';
 import axios from 'axios';
 import Wrapper from '../assets/wrappers/CocktailPage';
 import { isInputElement } from 'react-router-dom/dist/dom';
@@ -15,6 +15,8 @@ export const loader = async ({params}) => {
 
 const Cocktail = () => {
   const {id, data } = useLoaderData();
+
+  if(!data) return <Navigate to='/' />
 
   const singleDrink = data.drinks[0];
 
